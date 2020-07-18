056b7fb9e3752e71b8a11e4603e283fc1223396f
meriy100 <ttattataa@gmail.com>
2020-07-18 20:16:10 +0900

    [Add] drag

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,8 @@ module Main exposing (main)
 import Browser
 import Html as H exposing (Html)
 import Html.Attributes as A
-import Html.Events exposing (onClick)
+import Html.Events as E
+import Json.Decode as Decode exposing (Decoder)
 
 
 type Color
@@ -28,8 +29,13 @@ type Y
     | YSix
 
 
+type CubeId
+    = CubeId Int
+
+
 type alias Cube =
-    { color : Color
+    { id : CubeId
+    , color : Color
     , x : X
     , y : Y
     }
@@ -37,6 +43,7 @@ type alias Cube =
 
 type alias Model =
     { cubes : List Cube
+    , maybeDragOn : Maybe CubeId
     }
 
 
@@ -64,8 +71,10 @@ yList =
 init : Model
 init =
     { cubes =
-        List.map (\x -> { color = Black, x = x, y = YOne }) xList
-            ++ List.map (\x -> { color = White, x = x, y = YSix }) xList
+        List.map (\x -> { id = CubeId 0, color = Black, x = x, y = YOne }) xList
+            ++ List.map (\x -> { id = CubeId 0, color = White, x = x, y = YSix }) xList
+            |> List.indexedMap (\idx -> \cube -> { cube | id = CubeId idx })
+    , maybeDragOn = Nothing
     }
 
 
@@ -74,17 +83,68 @@ main =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = DragOn CubeId
+    | DragOut { x : X, y : Y }
+    | None
 
 
+update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Increment ->
+        None ->
             model
 
-        Decrement ->
-            model
+        DragOn cubeId ->
+            { model | maybeDragOn = Just cubeId }
+
+        DragOut point ->
+            let
+                maybeTargetCube =
+                    model.maybeDragOn
+                        |> Maybe.andThen
+                            (\cubeId ->
+                                model.cubes |> List.filter (\c -> c.id == cubeId) |> List.head
+                            )
+
+                newCubes =
+                    case maybeTargetCube of
+                        Nothing ->
+                            model.cubes
+
+                        Just targetCube ->
+                            model.cubes
+                                |> List.map
+                                    (\c ->
+                                        if c.id == targetCube.id then
+                                            { c | x = point.x, y = point.y }
+
+                                        else
+                                            c
+                                    )
+            in
+            { model | maybeDragOn = Nothing, cubes = newCubes }
+
+
+decodeDragStart : CubeId -> Decoder Msg
+decodeDragStart cubeId =
+    Decode.map (Debug.log "target")
+        (Decode.field "target" Decode.value)
+        |> Decode.andThen
+            (\x ->
+                Decode.succeed
+                    (DragOn cubeId)
+            )
+
+
+decodeDrop : { x : X, y : Y } -> Decoder ( Msg, Bool )
+decodeDrop point =
+    Decode.map (Debug.log "target")
+        (Decode.field "target" Decode.value)
+        |> Decode.andThen
+            (\x ->
+                Decode.succeed
+                    ( DragOut point, True )
+            )
 
 
 viewCube : Cube -> Html Msg
@@ -98,7 +158,12 @@ viewCube cube =
                 White ->
                     "cube__white"
     in
-    H.div [ A.class "cube", A.class colorClass ]
+    H.div
+        [ A.class "cube"
+        , A.class colorClass
+        , A.attribute "draggable" "true"
+        , cube.id |> decodeDragStart |> E.on "dragstart"
+        ]
         []
 
 
@@ -112,7 +177,11 @@ viewMass { x, y, cubes } =
         |> Maybe.andThen (viewCube >> List.singleton >> Just)
         |> Maybe.withDefault []
         |> H.div
-            [ A.class "mass-container" ]
+            [ A.class "mass-container"
+            , { x = x, y = y } |> decodeDrop |> E.preventDefaultOn "drop"
+            , E.preventDefaultOn "dragenter" (Decode.succeed ( None, True ))
+            , E.preventDefaultOn "dragover" (Decode.succeed ( None, True ))
+            ]
 
 
 view model =
