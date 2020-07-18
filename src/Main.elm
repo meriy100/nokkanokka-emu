38a8af3045cfb713c15525732752d7c049be4587
meriy100 <ttattataa@gmail.com>
2020-07-18 16:53:41 +0900

    [Add] models

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,102 @@
 module Main exposing (main)
+
 import Browser
-import Html exposing (Html, button, div, text)
+import Html as H exposing (Html)
 import Html.Events exposing (onClick)
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
 
-type Msg = Increment | Decrement
+type Color
+    = Black
+    | White
+
+
+type X
+    = XOne
+    | XTwo
+    | XThree
+    | XFour
+    | XFive
+
+
+type Y
+    = YOne
+    | YTwo
+    | YThree
+    | YFour
+    | YFive
+    | YSix
+
+
+type alias Cube =
+    { color : Color
+    , x : X
+    , y : Y
+    }
+
+
+type alias Model =
+    { cubes : List Cube
+    }
+
+
+xList : List X
+xList =
+    [ XOne
+    , XTwo
+    , XThree
+    , XFour
+    , XFive
+    ]
+
+
+yList : List Y
+yList =
+    [ YOne
+    , YTwo
+    , YThree
+    , YFour
+    , YFive
+    , YSix
+    ]
+
+
+init : Model
+init =
+    { cubes =
+        List.map (\x -> { color = Black, x = x, y = YOne }) xList
+            ++ List.map (\x -> { color = White, x = x, y = YSix }) xList
+    }
+
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model
+
+
+viewMass =
+    H.div []
+        []
+
 
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    H.div []
+        (yList
+            |> List.map
+                (\y ->
+                    xList |> List.map (\x -> viewMass)
+                )
+            |> List.concat
+        )
